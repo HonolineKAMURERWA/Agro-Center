c3e3d7611632fe82916342cb8b9abfe5ca9f24ed
Your name <you@example.com>
2023-05-07 23:50:52 +0200

    cart features

--- cart.js
+++ cart.js
@@ -1,4 +1,3 @@
-
 let openShopping = document.querySelector('.shopping');
 let closeShopping = document.querySelector('.closeShopping');
 let list = document.querySelector('.list');
@@ -17,9 +16,9 @@ closeShopping.addEventListener('click', ()=>{
 let products = [
     {
         id: 1,
-        name: 'NEEM POWDER',
-        image: 'images/1.png',
-        price: 30 
+        name: 'PRODUCT NAME 1',
+        image: '1.PNG',
+        price: 120000
     },
     {
         id: 2,
@@ -61,9 +60,7 @@ function initApp(){
             <img src="image/${value.image}">
             <div class="title">${value.name}</div>
             <div class="price">${value.price.toLocaleString()}</div>
-            <button onclick="addToCard(${key})">
-            <i class="bx bxs-cart-alt" style="color: #ffffff"></i>
-            </button>`;
+            <button onclick="addToCard(${key})">Add To Cart</button>`;
         list.appendChild(newDiv);
     })
 }
@@ -109,65 +106,3 @@ function changeQuantity(key, quantity){
     }
     reloadCard();
 }
-=======
-let buttonPrev = document.getElementById("prev-btn");
-let buttonNext = document.getElementById("next-btn");
-let person = document.querySelector(".client-details");
-let personOne = document.querySelector(".perso");
-
-buttonNext.addEventListener("click", nextPage);
-function nextPage() {
-  //   person.style.marginLeft = "-205px";
-  personOne.style.marginLeft = "-305px";
-  //   person.innerHTML = "kkkk";
-  console.log("heloooooo");
-}
-
-// const observer = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     console.log(entry);
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add("show");
-//     } else {
-//       entry.target.classList.remove("show");
-//     }
-//   });
-// });
-// let hiddenElements = document.querySelectorAll(".hidden");
-// hiddenElements.forEach((el) => observer.observe(el));
-
-const navbar = document.querySelector(".header");
-const logo = document.querySelector(".logo");
-const search = document.getElementById("search");
-const menu = document.getElementById("menu");
-const ul = document.querySelector(".lists");
-// const log = document.getElementById("logo");
-
-window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 200) {
-    navbar.classList.add("scrolled");
-    // search.innerHTML = '<i class="bx bx-search" style="color: #ffffff"></i>';
-    menu.innerHTML = '<i class="bx bx-menu" style="color: #749075"></i>';
-    menu.style.display = "block";
-    menu.style.float = "left";
-    menu.style.fontSize = "30px";
-    navbar.style.justfyContent = "space-around";
-    search.style.display = "none";
-    ul.style.display = "none";
-    logo.style.display = "none";
-  } else {
-    navbar.classList.remove("scrolled");
-    search.style.display = "flex";
-    menu.innerHTML = "";
-    menu.style.display = "none";
-    ul.style.display = "flex";
-    logo.style.display = "block";
-  }
-});
-
-let newMenu = document.querySelector(".short-menu");
-menu.addEventListener("click", function () {
-  console.log("hi");
-  // newMenu.style.display = "flex";
-});
-
